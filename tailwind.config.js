e8bbea64267f60f9c87248a5d6e9b5dfda38d590
ved0010 <109745529+ved0010@users.noreply.github.com>
2024-08-19 15:01:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this according to your project's structure
+    './public/index.html', // Include this if you have a public folder with HTML files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
